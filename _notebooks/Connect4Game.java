8e56041c2da9e6e91a253b61b472eedd42117d09
Soham360 <sohamkamat10@gmail.com>
2023-09-07 11:54:08 -0700

    e

--- _notebooks/Connect4Game.java
+++ _notebooks/Connect4Game.java
@@ -5,10 +5,10 @@ public class Connect4Game {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // Create a 2D array to represent the game grid
+        // create a 2D array to represent the game grid
         char[][] grid = new char[6][7];
 
-        // Initialize the array with empty spaces
+        // create the array with empty spaces
         for (int row = 0; row < grid.length; row++){
             for (int col = 0; col < grid[0].length; col++){
                 grid[row][col] = ' ';
@@ -19,7 +19,7 @@ public class Connect4Game {
         char player = 'X';
         boolean winner = false;        
 
-        // Play a turn
+        // play a turn
         while (winner == false && turn <= 42){
             boolean validPlay;
             int play;
@@ -29,12 +29,12 @@ public class Connect4Game {
                 System.out.print("Player " + player + ", choose a column: ");
                 play = in.nextInt();
 
-                // Validate the player's move
+                // make sure the player's move is valid
                 validPlay = validate(play,grid);
 
             }while (validPlay == false);
 
-            // Drop the checker into the selected column
+            // drop the icon into the selected column
             for (int row = grid.length-1; row >= 0; row--){
                 if(grid[row][play] == ' '){
                     grid[row][play] = player;
@@ -42,10 +42,10 @@ public class Connect4Game {
                 }
             }
 
-            // Determine if there is a winner
+            // determine if there is a winner
             winner = isWinner(player,grid);
 
-            // Switch players for the next turn
+            // swap players each turn
             if (player == 'X'){
                 player = 'O';
             } else{
@@ -54,6 +54,7 @@ public class Connect4Game {
 
             turn++;            
         }
+        // display the updated grid
         display(grid);
 
         if (winner){
@@ -68,7 +69,7 @@ public class Connect4Game {
 
     }
 
-    // Display the current state of the game grid
+    // display the current state of the game grid
     public static void display(char[][] grid){
         System.out.println(" 0 1 2 3 4 5 6");
         System.out.println("---------------");
@@ -85,14 +86,14 @@ public class Connect4Game {
         System.out.println();
     }
 
-    // Validate the player's move
+    // validate the player's move
     public static boolean validate(int column, char[][] grid){
-        // Check if the column is valid
+        // check if the column is valid
         if (column < 0 || column >= grid[0].length){
             return false;
         }
 
-        // Check if the column is full
+        // check if the column is full
         if (grid[0][column] != ' '){
             return false;
         }
@@ -100,9 +101,9 @@ public class Connect4Game {
         return true;
     }
 
-    // Check if a player has won
+    // check if a player has won
     public static boolean isWinner(char player, char[][] grid){
-        // Check for 4 in a row horizontally
+        // check for 4 in a row horizontally
         for(int row = 0; row < grid.length; row++){
             for (int col = 0; col < grid[0].length - 3; col++){
                 if (grid[row][col] == player &&
@@ -114,7 +115,7 @@ public class Connect4Game {
             }           
         }
 
-        // Check for 4 in a row vertically
+        // check for 4 in a row vertically
         for(int row = 0; row < grid.length - 3; row++){
             for(int col = 0; col < grid[0].length; col++){
                 if (grid[row][col] == player &&
@@ -126,7 +127,7 @@ public class Connect4Game {
             }
         }
 
-        // Check for 4 in a row diagonally (upward)
+        // check for 4 in a row diagonally (up right)
         for(int row = 3; row < grid.length; row++){
             for(int col = 0; col < grid[0].length - 3; col++){
                 if (grid[row][col] == player &&
@@ -138,7 +139,7 @@ public class Connect4Game {
             }
         }
 
-        // Check for 4 in a row diagonally (downward)
+        // Check for 4 in a row diagonally (up left)
         for(int row = 0; row < grid.length - 3; row++){
             for(int col = 0; col < grid[0].length - 3; col++){
                 if (grid[row][col] == player &&
